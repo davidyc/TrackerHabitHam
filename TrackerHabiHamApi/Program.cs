3a3924a8aa873887f43d0a17821ec3998e5ad432
Davidyc Laptop <davydovsergey.sd@gmail.com>
2026-03-09 16:13:07 +0500

    add new ui

--- TrackerHabiHamApi/Program.cs
+++ TrackerHabiHamApi/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using MongoDB.Driver;
@@ -10,7 +11,11 @@ using TrackerHabiHamApi.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
